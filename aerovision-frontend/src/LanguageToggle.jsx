676f1b497ec487d42d7c3758df6099754726dbf9
UST-NTC <tcngah@ust.hk>
2024-11-06 21:07:18 +0800

    Update LanguageToggle.jsx

--- aerovision-frontend/src/LanguageToggle.jsx
+++ aerovision-frontend/src/LanguageToggle.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 function LanguageToggle() {
   return (
-    <div data-layername="toggle" className="flex gap-2 items-center self-stretch my-auto">
-      <span data-layername="label1" className="self-stretch my-auto text-base text-white">
-        ENG
-      </span>
-      <div data-layername="switch" className="flex flex-col justify-center items-start self-stretch p-1 my-auto w-10 bg-white rounded-lg rotate-[3.141592653589793rad]">
-        <div data-layername="circle1" className="flex w-4 h-4 bg-purple-800 rounded min-h-[16px]" />
+    <div className="flex gap-8 justify-center items-center self-stretch my-auto">
+      <div className="flex gap-2 items-center self-stretch my-auto">
+        <span className="self-stretch my-auto text-base text-white">ENG</span>
+        <div className="flex flex-col justify-center items-start self-stretch p-1 my-auto w-10 bg-white rounded-lg rotate-[3.141592653589793rad]">
+          <div className="flex w-4 h-4 bg-purple-800 rounded min-h-[16px]" />
+        </div>
+        <span className="self-stretch my-auto text-base text-white">中</span>
       </div>
-      <span data-layername="label2" className="self-stretch my-auto text-base text-white">
-        中
-      </span>
     </div>
   );
 }
