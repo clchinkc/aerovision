f20c407ffbd21a1e36154b0ddbe288054012534a
UST-NTC <tcngah@ust.hk>
2024-11-06 20:06:54 +0800

    Language

---
+++ aerovision-frontend/WasteChart.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function WasteChart() {
+  return (
+    <div data-layername="螢幕截圖20241022下午954421" className="flex shrink-0 mt-6 h-[396px] max-md:max-w-full" />
+  );
+}
+
+export default WasteChart;
