267c1e4681fac3347f450882d10c2545a305ff35
UST-NTC <tcngah@ust.hk>
2024-11-06 21:07:41 +0800

    Update NavLink.jsx

--- aerovision-frontend/src/components/NavLink.jsx
+++ aerovision-frontend/src/components/NavLink.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function NavLink({ text }) {
   return (
-    <a href="#" data-layername="link" className="gap-1 self-stretch">
+    <a href="#" className="gap-1 self-stretch">
       {text}
     </a>
   );
